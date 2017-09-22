2a3675e22dd1a5e5a0b659210d8bbb012f19a43b
surface <jamsya@naver.com>
2017-09-22 16:59:33 +0900

    유투브 영상을 앱 내에서 재생한다.

---
+++ app/src/main/java/com/example/jamsy/smapleyoutubeapi/DeveloperKey.java
@@ -0,0 +1,22 @@
+package com.example.jamsy.smapleyoutubeapi;
+
+/**
+ * Created by jamsy on 2017-09-22.
+ */
+
+// Copyright 2012 Google Inc. All Rights Reserved.
+
+/**
+ * Static container class for holding a reference to your YouTube Developer Key.
+ */
+public class DeveloperKey {
+
+    /**
+     * Please replace this with a valid API key which is enabled for the
+     * YouTube Data API v3 service. Go to the
+     * <a href="https://code.google.com/apis/console/">Google APIs Console</a> to
+     * register a new developer key.
+     */
+    public static final String DEVELOPER_KEY = "AIzaSyDmaXanAKeyf4Q91e6lr0wucRRGdiPDKj8";
+
+}
